40074b772070d43033879bfd8f1edd6bc8a685e0
Greg Desplaces <ontact@gregdesplaces.com>
2022-05-31 13:00:25 +0200

    Init

--- js/greg-plugin-front.js
+++ js/greg-plugin-front.js
@@ -1,19 +1,16 @@
 (function($) {
 
-if (settings.isActive) {
-        $('<div></div>')
-                .attr('id', 'greg-plugin-container')
-                .append(`<p>${settings.content}</p>`)
-                .css({
-                'background-color': settings.bgColor,
-                'color': settings.textColor,
-                })
-                .appendTo('body');
+        if (settings.isActive) {
+                $('<div></div>')
+                        .attr('id', 'greg-plugin-container')
+                        .append(`<p>${settings.content}</p>`)
+                        .css({
+                        'background-color': settings.bgColor,
+                        'color': settings.textColor,
+                        })
+                        .appendTo('body');
 
-        (settings.position === "bottom") ? $('#greg-plugin-container').css('bottom', '0') : $('#greg-plugin-container').css('top', '0');
-}
-
-
-
+                (settings.position === "bottom") ? $('#greg-plugin-container').css('bottom', '0') : $('#greg-plugin-container').css('top', '0');
+        }
 
 })(jQuery);
